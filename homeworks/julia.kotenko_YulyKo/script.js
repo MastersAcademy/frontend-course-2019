d37dcd3af66a18ddfad36599538d08de3d6598fb
YulyKo <juliakotenko.forstudy@gmail.com>
2019-11-05 01:28:07 +0200

    task-1 in task-0: replaced and added methods for get value from inputs and textarea

--- homeworks/julia.kotenko_YulyKo/script.js
+++ homeworks/julia.kotenko_YulyKo/script.js
@@ -4,37 +4,46 @@ function onSubmit() {
     checkEmptyInputs()
 }
 
+function getEmailValue() {
+    let email = document.querySelector('#email')
+    return email.value
+}
+
+function getNameValue() {
+    let name = document.querySelector('#name')
+    return name.value
+}
+
+function getMessageValue() {
+    let message = document.querySelector('#message')
+    return message.value
+}
 
 function checkEmptyInputs() {
-    let email = document.querySelector('#email')
-    let name = document.querySelector('#name')
-    let message = document.querySelector('#message')
-
-    if (email.value != '' && name.value != '' && message.value != '') {
-        validationOfEmail(email.value, name.value)
-        return true;
+    email = getMessageValue()
+    name = getNameValue()
+    message = getMessageValue()
+    if (email != '' && name != '' && message != '') {
+        validationOfEmail()
     } else {
-        alert('Seriously? XD\nIt won\'t work that way.\nWrite information in all inputs')
-        return false;
+        alert('It won\'t work that way.\nWrite information in all inputs')
     }
 }
 
-function validationOfEmail(email, name) {
+function validationOfEmail() {
     var mailformat = /\S+@\S+\.\S+/
-    if (email.match(mailformat)) {
+    if (getEmailValue.match(mailformat)) {
         return validationName(name)
     } else {
         alert("Your e-mail is not correct")
-        return false
     }
 }
 
 
-function validationName(name) {
-    if (/^[A-Za-z\s]+$/.test(name)) {
+function validationName() {
+    if (/^[A-Za-z\s]+$/.test(getNameValue)) {
         return true
     } else {
         alert("Your name is not correct written.\nRemove numbers and special symbols from input")
-        return false
     }
 }
