a88f0a528bdcc5db620d3bcd619b90a68d69ea8c
Maxim Shikunov <schikk@ukr.net>
2019-11-07 10:06:40 +0200

    Modify form validation - Homework 2

--- homeworks/maksym.shykunov_schikk/homework_1/js/index.js
+++ homeworks/maksym.shykunov_schikk/homework_1/js/index.js
@@ -1,52 +1,48 @@
 (function () {
     
 	const form = document.querySelector('#main_form');
-	const formInputs = document.querySelectorAll('input, textarea');
-	const nameInput = document.querySelector('#user_name');
-	const emailInput = document.querySelector('#user_email');
-	const mssgInput = document.querySelector('#user_mssg');
+	const fields = document.querySelectorAll('.validate');
 	const formObj = {};
-
-    function validateName(name) {
-        var nameReg = /^[a-zA-Zа-яА-Я ]{2,3000}$/;
-        return nameReg.test( name );
-    }
-    function validateEmail(email) {
-        var emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return emailReg.test( email );
-    }  
+	const validate = {
+		name: {
+			regExp: /^[a-zA-Zа-яА-Я ]{2,3000}$/,
+			errorMessage: 'Minimum 2 characters'
+		},
+		email: {
+			regExp: /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i,
+			errorMessage: 'Email is not valid'
+		},
+		mssg: {
+			regExp: /./,
+			errorMessage: 'Require field'
+		} 
+	};
 
 	form.addEventListener("submit" , function() {
 		event.preventDefault();
-		// Get form values
-	    for (let i = 0; i < formInputs.length; i++) {
-			let item = formInputs.item(i);
-			formObj[item.name] = item.value;
-	    }
-	    // Validate form and send form values
-	    if (validateName(nameInput.value) && validateEmail(emailInput.value) && mssgInput.value.length > 0 ) {
-	    	alert(JSON.stringify(formObj));
-	    	console.log(formObj);
-	    } else {
-            if ( validateName(nameInput.value) == false ){
-				nameInput.classList.add("invalid");
-				setTimeout(function(){
-					nameInput.classList.remove("invalid");
-				}, 1000);
-            }
-            if ( validateEmail(emailInput.value) == false ){
-				emailInput.classList.add("invalid");
-				setTimeout(function(){
-					emailInput.classList.remove("invalid");
-				}, 1000);
-            }
-            if (mssgInput.value.length <= 0) {
-            	mssgInput.classList.add("invalid");
-            	setTimeout(function(){
-					mssgInput.classList.remove("invalid");
-				}, 1000);
-            }
-	    }
+		let valid = true;
+		for (let i=0; i < fields.length; i++) {
+			const nameAttr = fields[i].getAttribute('name');
+			const valueInputs = fields[i].value;
+			const parent = fields[i].parentNode;
+			const errorText = parent.querySelector('.error-mssg');
+			errorText && errorText.remove();
+			
+			if(!validate[nameAttr].regExp.test(valueInputs)) {
+				valid = false;
+				const errorContainer = document.createElement('span');
+				errorContainer.classList.add('error-mssg');
+				errorContainer.textContent = validate[nameAttr].errorMessage;
+				parent.appendChild(errorContainer);
+			} else {
+				formObj[nameAttr] = valueInputs;
+			}
+		}
+		if(valid) {
+			alert(JSON.stringify(formObj));
+			console.log(formObj);
+		}
+		
 	});
 
 }());
