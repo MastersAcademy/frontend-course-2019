c52bf0acb33bf486ecd5b046cd55286bc61652f2
AlexKuskov <a.kuskov98@gmail.com>
2019-11-08 13:21:57 +0200

    Move regex pattern variables to the top of the index.js file

--- homeworks/alexandr.kuskov_AlexKuskov/homework_markup_1/index.js
+++ homeworks/alexandr.kuskov_AlexKuskov/homework_markup_1/index.js
@@ -2,11 +2,10 @@ const formInputs = document.querySelectorAll('.form__input');
 const messagesTextarea = document.querySelector('.form__messages-textarea');
 const sendMessageButton = document.querySelector('.form__send-messages-btn');
 
-sendMessageButton.addEventListener('click', (event) => {
-    // this pattern matches cases when only name or name + surname is inputed
-    const namePattern = /^[A-Za-z]+( [A-Za-z]+)*$/;
-    const emailPattern = /^\S+@\S+\.\S+$/;
+const namePattern = /^[A-Za-z]+( [A-Za-z]+)*$/;
+const emailPattern = /^\S+@\S+\.\S+$/;
 
+sendMessageButton.addEventListener('click', (event) => {
     const isInputNameValid = formInputs[0].value.match(namePattern);
     const isInputEmailValid = formInputs[1].value.match(emailPattern);
 
