db3fa0f5125011f29852d82ee1391df7977eca4d
Anton <omega24v1993@gmail.com>
2019-11-13 01:15:49 +0200

    fixed js

--- homeworks/anton.sergienko_omega24v/homework_js_1/main.js
+++ homeworks/anton.sergienko_omega24v/homework_js_1/main.js
@@ -1,9 +1,5 @@
 (() => {
     class HTTPClient {
-        constructor() {
-
-        }
-        
         get(url, options) {
             return new Promise((resolve, reject) => {
                 const request = new XMLHttpRequest();
