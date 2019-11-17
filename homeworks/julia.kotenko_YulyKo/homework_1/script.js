9943e36e5283eb574009abf93c66c6d14d3bdc30
y.kotenko <kyabi71@gmail.com>
2019-11-17 18:15:37 +0200

    task-1: fixed js

--- homeworks/julia.kotenko_YulyKo/homework_1/script.js
+++ homeworks/julia.kotenko_YulyKo/homework_1/script.js
@@ -1,36 +1,58 @@
-let email = '';
-let name = '';
-let message = '';
 const NAME_FORMAT = /^[A-Za-z\s]+$/;
 const MAIL_FORMAT = /\S+@\S+\.\S+/;
 
-function showResultMessage() {
-    alert(`User ${name}\n(${email})
-Your message: ${message} was sended to Jhon Smith`);
+class ContactMeForm {
+    constructor() {
+        this.form = document.querySelector('form');
+        this.email = this.form.querySelector('#email');
+        this.name = this.form.querySelector('#name');
+        this.message = this.form.querySelector('#message');
+        this.form.querySelector('#submit').addEventListener('click', () => this.onSubmit());
+    }
+
+    getEmailValue() {
+        return this.email.value;
+    }
+
+    getNameValue() {
+        return this.name.value;
+    }
+
+    getMessageValue() {
+        return this.message.value;
+    }
+
+    showResultMessage() {
+        const name = this.getNameValue();
+        const email = this.getEmailValue();
+        const message = this.getMessageValue();
+        alert(`User ${name}\n(${email})\nYour message: ${message} was sanded to John Smith`);
+    }
+
+    validateName() {
+        const name = this.getNameValue();
+        if (!NAME_FORMAT.test(name)) {
+            return 'Your name is not correct';
+        }
+        return '';
+    }
+
+    validateEmail() {
+        const email = this.getEmailValue();
+        if (!email.match((MAIL_FORMAT))) {
+            return 'You have problem with email';
+        }
+        return '';
+    }
+
+    onSubmit() {
+        const validationEmailError = this.validateEmail();
+        const validationNameError = this.validateName();
+        if (validationEmailError === '' || validationNameError === '') {
+            return '';
+        }
+        return this.showResultMessage();
+    }
 }
 
-function validateName() {
-    if (!NAME_FORMAT.test(name)) {
-        return 'Your name is not correct';
-    }
-    return '';
-}
-
-function validateEmail() {
-    if (!email.match((MAIL_FORMAT))) {
-        return 'You have problem with email';
-    }
-    return '';
-}
-
-function onSubmit() {
-    email = document.querySelector('#email').value;
-    name = document.querySelector('#name').value;
-    message = document.querySelector('#message').value;
-    const validationError = validateEmail() || validateName();
-    if (validationError) {
-        alert(validationError);
-    }
-    return showResultMessage();
-}
-document.querySelector('#submit').addEventListener('click', onSubmit);
+new ContactMeForm();
