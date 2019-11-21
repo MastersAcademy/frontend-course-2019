e7a8be6d451fa6046841633e976ece6aa6e5fa31
batteryfree <battery.free@gmail.com>
2019-11-21 13:27:57 +0200

    fixed app.js

--- homeworks/roman.vlasov_batteryfree/homework_2/app.js
+++ homeworks/roman.vlasov_batteryfree/homework_2/app.js
@@ -16,39 +16,44 @@ class SendMessage {
     }
 
     listenEvents() {
-        this.form.addEventListener('submit', (element) => {
-            element.preventDefault();
+        this.elements.form.addEventListener('submit', (event) => {
+            event.preventDefault();
             this.validate();
-            if (this.isValid) this.sendMessage();
+            if (this.isValid) alert('Message send');
         });
     }
 
-    isEmptyField(text) {
-        return !text.trim();
+    static isEmptyField(input) {
+        return !input.value.trim();
     }
 
-    isEmail(text) {
+    static isEmail(input) {
         const EMAIL_PATTERN = /^([a-z0-9_-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-        return !EMAIL_PATTERN.test(text);
+        return !EMAIL_PATTERN.test(input.value);
     }
 
-    isName(text) {
+    static isName(input) {
         const NAME_PATTERN = /^([a-zA-Z])+\s{1}([a-zA-Z])+$/;
-        return !NAME_PATTERN.test(text);
+        return !NAME_PATTERN.test(input.value);
     }
 
     validateName() {
-        if (this.isEmptyField(this.elements.name.value)) return '\'Name\' fiels is required.\n';
-        if (this.isName(this.elements.name.value)) return '\'Name\' has not valid format.\n';
+        if (SendMessage.isEmptyField(this.elements.name)) return '\'Name\' fiels is required.\n';
+        if (SendMessage.isName(this.elements.name)) return '\'Name\' has not valid format.\n';
+        return '';
     }
 
     validateEmail() {
-        if (this.isEmptyField(this.elements.email.value)) return '\'Email\' fiels is required.\n';
-        if (this.isEmail(this.elements.email.value)) return '\'Email\' has not valid format.\n';
+        if (SendMessage.isEmptyField(this.elements.email)) return '\'Email\' fiels is required.\n';
+        if (SendMessage.isEmail(this.elements.email)) return '\'Email\' has not valid format.\n';
+        return '';
     }
 
     validateMessage() {
-        if (this.isEmptyField(this.elements.message.value)) return '\'Messages\' fiels is required.\n';
+        if (SendMessage.isEmptyField(this.elements.message)) {
+            return '\'Messages\' fiels is required.\n';
+        }
+        return '';
     }
 
     validate() {
@@ -57,24 +62,20 @@ class SendMessage {
         const validationErrorEmail = this.validateEmail();
         const validationErrorMessage = this.validateMessage();
 
-        this.renderValidationState(this.elements.name, !validationErrorName);
-        this.renderValidationState(this.elements.email, !validationErrorEmail);
-        this.renderValidationState(this.elements.message, !validationErrorMessage);
+        SendMessage.renderValidationState(this.elements.name, !validationErrorName);
+        SendMessage.renderValidationState(this.elements.email, !validationErrorEmail);
+        SendMessage.renderValidationState(this.elements.message, !validationErrorMessage);
 
-        if (vErrorName) validationError = validationErrorName;
-        if (vErrorEmail) validationError += validationErrorEmail;
-        if (vErrorMessage) validationError += validationErrorMessage;
+        if (validationErrorName) validationError = validationErrorName;
+        if (validationErrorEmail) validationError += validationErrorEmail;
+        if (validationErrorMessage) validationError += validationErrorMessage;
 
         this.isValid = !validationError;
 
         if (!this.isValid) alert(validationError);
     }
 
-    sendMessage() {
-        alert('Message send');
-    }
-
-    renderValidationState(input, toggle) {
+    static renderValidationState(input, toggle) {
         input.classList.toggle('contact__invalid', !toggle);
     }
 }
