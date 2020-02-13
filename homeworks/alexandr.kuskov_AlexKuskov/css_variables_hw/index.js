1300ae7b7de8dbc832fd12ba651074d85a1ecf44
Alex <a.kuskov98@gmail.com>
2020-02-13 23:06:55 +0200

    add 2 additional themes for the page

---
+++ homeworks/alexandr.kuskov_AlexKuskov/css_variables_hw/index.js
@@ -0,0 +1,32 @@
+const root = document.documentElement;
+const themeSelector = document.querySelector('.js-themes');
+
+function handleThemeUpdate(e) {
+    const selectEl = e.target;
+    const selectedOptionIndex = selectEl.selectedIndex;
+    const selectedOption = selectEl.options[selectedOptionIndex];
+
+    switch (selectedOption.value) {
+        case 'original':
+            root.style.setProperty('--global--color--primary', '#666');
+            root.style.setProperty('--global--color--secondary', '#FE7E11');
+            root.style.setProperty('--global--color--theme-primary', '#FFF');
+            root.style.setProperty('--global--color--theme-secondary', '#F0F0F0');
+            break;
+        case 'dark':
+            root.style.setProperty('--global--color--primary', '#FFF');
+            root.style.setProperty('--global--color--secondary', '#00F');
+            root.style.setProperty('--global--color--theme-primary', '#000');
+            root.style.setProperty('--global--color--theme-secondary', '#545454');
+            break;
+        case 'turquoise':
+            root.style.setProperty('--global--color--primary', '#000');
+            root.style.setProperty('--global--color--secondary', '#3baf00');
+            root.style.setProperty('--global--color--theme-primary', '#c9eae9');
+            root.style.setProperty('--global--color--theme-secondary', '#98ded7');
+            break;
+        default:
+    }
+}
+
+themeSelector.addEventListener('change', handleThemeUpdate);
